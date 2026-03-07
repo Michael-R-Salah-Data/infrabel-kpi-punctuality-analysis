e5ffcce396feff203d4b14e95449fa3ce1e1acf6
Michael Salah <salahmichael@hotmail.com>
2026-03-07 17:13:53 +0100

    build: set python_requires in setup file

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    python_requires=">=3.10",
     install_requires=[
         "requests",
         "tqdm"
