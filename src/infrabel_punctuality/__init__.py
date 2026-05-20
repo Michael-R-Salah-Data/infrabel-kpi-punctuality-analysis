a5d759a1261670cb7ab70c099565c55e31b9704a
Michael Salah <salahmichael@hotmail.com>
2026-05-20 16:09:24 +0200

    feat: add function to strip whitespace from dataframes

--- src/infrabel_punctuality/__init__.py
+++ src/infrabel_punctuality/__init__.py
@@ -1,2 +1,2 @@
 from .ingestion import prepare_download, check_output_dir, download_dataset, generate_file_registry, run_download
-from .utils import clean_column_string, clean_df_string
+from .utils import clean_column_string, clean_df_string, strip_df_string
--- src/infrabel_punctuality/utils.py
+++ src/infrabel_punctuality/utils.py
@@ -24,7 +24,7 @@ def clean_column_string(s: pd.Series):
 
 def clean_df_string(df: pd.DataFrame, columns: list[str]):
     """
-    Clean a pandas DataFrame or a list of column names by stripping leading and trailing whitespace,
+    Clean a list of selected columns of a pandas DataFrame by stripping leading and trailing whitespace,
       and converting characters to lowercase.
 
     Arguments: 
@@ -47,3 +47,26 @@ def clean_df_string(df: pd.DataFrame, columns: list[str]):
         df[col] = df[col].str.lower().str.strip()
     return df
 
+def strip_df_string(df: pd.DataFrame, columns: list[str]):
+    """
+    Strip leading, trailing, and double whitespaces of selected columns of a pandas DataFrame.
+
+    Args: 
+        df: pd.DataFrame containing the columns to strip.
+        columns(list[str]): A list of column names to strip. 
+            Each column must contain string values (dtype 'object' or 'string').
+    Returns:
+        df: pd.DataFrame with the columns stripped.
+    Raises:
+        TypeError: If df is not a pd.DataFrame, or if any column does not contain string values.
+
+    """
+    if not isinstance (df, pd.DataFrame):
+        raise TypeError(f"Expected a pandas DataFrame, got {type(df).__name__}")
+    
+    for col in columns:
+        if not pd.api.types.is_string_dtype(df[col]):
+            raise TypeError(f"Column {col} is not a string column, got {df[col].dtype}")
+        
+        df[col] = df[col].str.replace(r"\s+", " ", regex=True).str.strip()
+    return df
