6f93d03f2658c24e78036eaa439bff976aebeec9
Michael Salah <salahmichael@hotmail.com>
2026-06-14 11:51:36 +0200

    feat(03_04_sql): add DDL for train service dimension

---
+++ sql/ddl/03_04_create_dim_train_service.sql
@@ -0,0 +1,15 @@
+-- Execute this script within the [infrabel_punctuality_dwh] database
+
+-- SK_Train_Service is loaded directly from the Dim_Train_Service pandas DataFrame
+
+CREATE TABLE dim.Dim_Train_Service(
+	SK_Train_Service INT NOT NULL,
+	Operator_Train_Service NVARCHAR(20) NOT NULL,
+	Relation_Category NVARCHAR(20) NOT NULL,
+	Relation NVARCHAR(20) NOT NULL,
+	Relation_Direction NVARCHAR(100) NOT NULL,
+	Direction_Is_Inferred BIT NOT NULL,
+	Is_Local BIT NOT NULL,
+	Is_National BIT NOT NULL,
+	CONSTRAINT PK_Service PRIMARY KEY (SK_Train_Service)
+	)
