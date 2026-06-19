e6bc10b1c5da7b3a5d42e99e3a5fd56edd23d4d9
Michael Salah <salahmichael@hotmail.com>
2026-06-19 09:31:59 +0200

    feat(03_02_sql): extend time granularity to second

--- sql/ddl/03_02_create_dim_time.sql
+++ sql/ddl/03_02_create_dim_time.sql
@@ -1,52 +1,60 @@
 -- Execute this script within the [infrabel_punctuality_dwh] database
 
-
 DROP TABLE IF EXISTS dim.Dim_Time;
 
 CREATE TABLE dim.Dim_Time (
-    TimeId INT PRIMARY KEY,
-    Time TIME NOT NULL,     
+    TimeKey INT NOT NULL,              
+    FullTime TIME(0) NOT NULL,          
     Hour INT NOT NULL,
     Minute INT NOT NULL,     
-    TimePeriod NVARCHAR(30) NOT NULL,
-    IsPeakHour BIT NOT NULL
+    Second INT NOT NULL,                
+    TimePeriod NVARCHAR(30) NOT NULL,   
+    IsPeakHour BIT NOT NULL,            
+    CONSTRAINT PK_Time PRIMARY KEY (TimeKey)
 );
 
-DECLARE @TimeId INT = 0;
+DECLARE @TimeKey INT = 0;               
 DECLARE @Hour INT = 0;
 DECLARE @Minute INT = 0;
+DECLARE @Second INT = 0;                
 DECLARE @TimePeriod NVARCHAR(30);
 
 WHILE @Hour < 24
 BEGIN
-    SET @Minute = 0;
-
+    -- Railway peakhours
     SET @TimePeriod = CASE
         WHEN @Hour BETWEEN 6 AND 9 THEN 'AM_Peak'
         WHEN @Hour BETWEEN 10 AND 15 THEN 'Midday_OffPeak'
         WHEN @Hour BETWEEN 16 AND 19 THEN 'PM_Peak'
-		WHEN @Hour BETWEEN 20 AND 23 THEN 'Evening_OffPeak'
+        WHEN @Hour BETWEEN 20 AND 23 THEN 'Evening_OffPeak'
         ELSE 'Night'
     END;
 
+    SET @Minute = 0;
     WHILE @Minute < 60
     BEGIN
-        SET @TimeId = (@Hour * 100) + @Minute;
+        
+        SET @Second = 0;
+        WHILE @Second < 60
+        BEGIN
 
-        INSERT INTO dim.Dim_Time(TimeId, [Time], [Hour], [Minute], TimePeriod, IsPeakHour)
+            INSERT INTO dim.Dim_Time(TimeKey, FullTime, [Hour], [Minute], [Second], TimePeriod, IsPeakHour)
+            VALUES (
+                @TimeKey,                                       
+                TIMEFROMPARTS(@Hour, @Minute, @Second, 0, 0),   
+                @Hour,
+                @Minute,
+                @Second,
+                @TimePeriod,
+                CASE WHEN @Hour BETWEEN 6 AND 9 OR @Hour BETWEEN 16 AND 19 THEN 1 ELSE 0 END
+            );
 
-        VALUES (
-        @TimeId,
-        TIMEFROMPARTS(@Hour, @Minute, 0, 0, 0),
-        @Hour,
-        @Minute,
-        @TimePeriod,
-        CASE WHEN @Hour BETWEEN 6 AND 9 OR @Hour BETWEEN 16 AND 19 THEN 1 ELSE 0 END
-        )
+            SET @TimeKey += 1; 
+            SET @Second += 1;
+        END
 
         SET @Minute += 1;
     END
-   
 
     SET @Hour += 1;
 END;
