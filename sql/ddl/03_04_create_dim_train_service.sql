d5c6bbf10a86c060530c04889aa8bb9bd950e09e
Michael Salah <salahmichael@hotmail.com>
2026-06-20 18:14:21 +0200

    feat(03_04_sql): add existence check before create table

--- sql/ddl/03_04_create_dim_train_service.sql
+++ sql/ddl/03_04_create_dim_train_service.sql
@@ -2,14 +2,17 @@
 
 -- SK_Train_Service is loaded directly from the Dim_Train_Service pandas DataFrame
 
-CREATE TABLE dim.Dim_Train_Service(
-	SK_Train_Service INT NOT NULL,
-	Operator_Train_Service NVARCHAR(20) NOT NULL,
-	Relation_Category NVARCHAR(20) NOT NULL,
-	Relation NVARCHAR(20) NOT NULL,
-	Relation_Direction NVARCHAR(100) NOT NULL,
-	Direction_Is_Inferred BIT NOT NULL,
-	Is_Local BIT NOT NULL,
-	Is_National BIT NOT NULL,
-	CONSTRAINT PK_Service PRIMARY KEY (SK_Train_Service)
-	)
+IF OBJECT_ID('dim.Dim_Station', N'U') IS NULL
+BEGIN
+	CREATE TABLE dim.Dim_Train_Service(
+		SK_Train_Service INT NOT NULL,
+		Operator_Train_Service NVARCHAR(20) NOT NULL,
+		Relation_Category NVARCHAR(20) NOT NULL,
+		Relation NVARCHAR(20) NOT NULL,
+		Relation_Direction NVARCHAR(100) NOT NULL,
+		Direction_Is_Inferred BIT NOT NULL,
+		Is_Local BIT NOT NULL,
+		Is_National BIT NOT NULL,
+		CONSTRAINT PK_Service PRIMARY KEY (SK_Train_Service)
+		)
+END;
